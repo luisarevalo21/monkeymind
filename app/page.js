1ffbb981abffb555dea2cc6991c2e8071bbd23c2
ozanyusufoglu <ozanyusufoglu@gmail.com>
2023-04-10 16:17:26 +0300

    main components created

--- app/page.js
+++ app/page.js
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import { Space_Grotesk } from "next/font/google";
+import TimerControl from "./components/timer/TimerControl";
+import Todo from "./components/todo/Todo";
 
 import styles from "./page.module.css";
+import TimerExtension from "./components/timer/TimerControl";
 
 const font = Space_Grotesk({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={font.className}>
-      <h1>Hello Monkey Mind</h1>
+      <TimerControl />
+      <Todo />
     </main>
   );
 }
